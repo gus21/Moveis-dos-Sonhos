c31a4bd98e17ef445264bb907c71741bad82a7c1
Tuanno <tuannodanyllo26@gmail.com>
2018-03-22 06:26:05 -0300

    alterado o css do cadastro para centralizar o formulário e o botão

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Cadastro</title>
-
+	<link rel="stylesheet" type="text/css" href="css/cadastro.css">
+	
 </head>
 
 <body>
@@ -13,11 +14,11 @@
  ?>	
 
 
-<div>
+<div id="form">
   <form action="formu.php" method="POST">
-    <input type="text" name="Usuario" placeholder="Usuario" required title="Nome de Usuario"><p>
-    <input type="password" name="pass" placeholder="Senha" required ><p>
-    <input type="submit" value="Entrar" >
+    <input type="text" name="Usuario" placeholder="Usuario" required title="Nome de Usuario" class="c"><p>
+    <input type="password" name="pass" placeholder="Senha" required class="c"><p>
+    <input type="submit" value="Cadastrar" class="b">
   </form>
 </div>
 
