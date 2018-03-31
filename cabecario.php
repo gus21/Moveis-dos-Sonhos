3620ffcd5ca02da7f842aaefee65e7e1530845c5
Pancosta <dz6.gustav@gmail.com>
2018-03-31 17:34:58 -0300

    botão sair desaparece quand^Cusuário não está logado

--- cabecario.php
+++ cabecario.php
@@ -5,12 +5,12 @@
 	<title>Móveis dos Sonhos</title>
 	<link rel="stylesheet" type="text/css" href="css/cabecario.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php session_start(); ?>
+	<?php session_start(); ?>
 	<table class="table">
 		<tr>
 			<th class="th">
@@ -19,32 +19,36 @@
 				</h1>
 			</th>
 			<td class="td"><p>
-			<?php if(isset($_SESSION['user'])) : ?>
-				<?php echo "Olá, ". $_SESSION['user']; ?>
-			<?php endif ?>
-			<?php if (!isset($_SESSION['user'])) :?>
-				<a href="login.php" class="a"><h3>Entrar</h3></a>
-			<?php endif ?>
-			 </p>
+				<?php if(isset($_SESSION['user'])) : ?>
+					<?php echo "Olá, ". $_SESSION['user']; ?>
+				<?php endif ?>
+				<?php if (!isset($_SESSION['user'])) :?>
+					<a href="login.php" class="a"><h3>Entrar</h3></a>
+				<?php endif ?>
+			</p>
 			
-	</td>
-	<td class="td">
-		<a href="cadastro.php" class="a"><h3>Cadastrar</h3></a>
-	</td>			
-	<td class="td">
-		<a href="sobre.php" class="a"><h3>Sobre</h3></a>
-	</td>
-	 <td class="td">
-    	<a href="produtos.php" class="a"><h3>Produtos</h3></a>
-    </td>
-	<td class="td">
-		<a href="sair.php" class="a">
-          <center><span class="glyphicon glyphicon-log-out" ></span><center>
-        </a>
-    </td>
-   
-</tr>
-</table>	
+		</td>
+		<td class="td">
+			<a href="cadastro.php" class="a"><h3>Cadastrar</h3></a>
+		</td>			
+		<td class="td">
+			<a href="sobre.php" class="a"><h3>Sobre</h3></a>
+		</td>
+		<td class="td">
+			<a href="produtos.php" class="a"><h3>Produtos</h3></a>
+		</td>
+		<?php if (isset($_SESSION['user'])) :?>
+			<td class="td">
+				<a href="sair.php" class="a">
+					<center><span class="glyphicon glyphicon-log-out" ></span><center>
+					</a>
+				</td>
+
+		<?php endif ?>
+
+
+		</tr>
+	</table>	
 
 </body>
 </html>
