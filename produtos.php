f4dbbc5bc9d0827d4bc78d7326d179264759df6f
gus21 <gustavostsmo7@gmail.com>
2018-04-03 21:12:01 -0300

    colocando a pagina produtos contem um bug

--- produtos.php
+++ produtos.php
@@ -21,12 +21,32 @@
 				<img src="img/produtos/mesa-madeira-p.png">
 				<a href=""><h3>Mesa de madeira pequena</h3></a>
 			</td>
+			<td>
+				<img src="img/produtos/mesa-jantar-vidro.jpg" width="200" height="200">
+				<a href=""><h3>Mesa de jantar de vidro</h3></a>
+			</td>
+			<td>
+				<img src="img/produtos/mesa-jantar-marmore.jpg" width="200" height="200">
+				<a href=""><h3>Mesa de jantar</h3></a>
+			</td>
 		</tr>
 		<tr>
 			<td>
-				
+				<img src="img/produtos/cadeira-de-balanço.jpg" width="200" height="200">
+				<a href=""><h3>Cadeira de balanço</h3></a>
 			</td>
-			<td></td>
+			<td>
+				<img src="img/produtos/poltrona-executiva.png" width="200" height="200">
+				<a href=""><h3>Poltrona executiva</h3></a>
+			</td>
+			<td>
+				<img src="img/produtos/cadeira-executiva.png" width="200" height="200">
+				<a href=""><h3>Cadeira executiva</h3></a>
+			</td>
+			<td>
+				<img src="img/produtos/sofa.png" width="300" height="200">
+				<a href=""><h3>Sofa Medio</h3></a>
+			</td>
 		</tr>
 	</table>
 	<?php include 'rodape.php'; ?>
