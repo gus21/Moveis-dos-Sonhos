a47ee787e59dc1eb613ba5d544a1e2f1ceccc637
Tuanno <tuannodanyllo26@gmail.com>
2018-04-10 20:35:16 -0300

    Melhoria na pagina de produtos

--- produtos.php
+++ produtos.php
@@ -1,38 +1,166 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Produtos</title>
-	<link rel="stylesheet" type="text/css" href="css/produtos.css">
-</head>
-<body>
-	<?php include 'cabecario.php'; ?>
-	<div>
-		<div class="imagem">
-			<center>
-				<img src="img/produtos/mesa-centro.jpg" >
-				<h3>Mesa centro de madeira</h3>
-			</center>		
-		</div>
-		<div class="imagem">
-			<center>
-				<img src="img/produtos/mesa-xadrez.jpg" >
-				<h3>Mesa para jogar xadrez</h3>
-			</center>
-		</div>
-		<div class="imagem">
-			<center>
-				<img src="img/produtos/mesa-madeira-p.png">
-				<h3>Mesa de madeira pequena</h3>
-			</center>		
-		</div>
-		<div class="imagem">
-			<center>
-				<img src="img/produtos/mesa-jantar-vidro.jpg">
-				<h3>Mesa de jantar de vidro</h3>
-			</center>	
-		</div>
-	</div>
-	<?php include 'rodape.php'; ?>
-</body>
+    <head> 
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Pacifico&subset=latin-ext,vietnamese" rel="stylesheet">
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/produtos.css">
+		
+		<title>Produtos</title>
+	</head>
+	<body>
+		<?php
+		include 'cabecario.php';
+		?>
+		
+		<div class="container">
+		
+	<div class="row">
+	
+    	
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/mesa-centro.jpg" alt="...">
+      			<h4>MESA DE CENTRO</h4>
+      			<p></p>
+      			
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$300,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/mesa-jantar-marmore.jpg" alt="...">
+      			<h4>MESA DE JANTAR</h4>
+      			<p></p>
+      			
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$350,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/mesa-xadrez.jpg" alt="...">
+      			<h4>MESA DE XADREZ</h4>
+      			<p></p>
+      			
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$250,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/cadeira-de-balanço.jpg" alt="...">
+      			<h4>CADEIRA DE BALANÇO</h4>
+      			<p></p>
+      			
+      			<div class="row ">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$200,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/sofa2.png" alt="...">
+      			<h4>SOFÁ</h4>
+      			<p></p>
+      			
+      			<div class="row ">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$450,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/cadeira-executiva2.png" alt="...">
+      			<h4>CADEIRA EXECUTIVA</h4>
+      			<p></p>
+      			
+      			<div class="row ">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$200,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/poltrona-executiva.png" alt="...">
+      			<h4>POLTRONA EXECUTIVA</h4>
+      			<p></p>
+      			
+      			<div class="row ">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$180,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" >Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-3 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="img/produtos/mesa-madeira-p-2.png" alt="...">
+      			<h4>MESA DE MADEIRA</h4>
+      			<p></p>
+      			
+      			<div class="row ">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="price">$200,00</p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      				 <a href="" >	<button class="btn btn-info right" > Novo Item</button></a>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>  		
+	</body>
 </html>
+  
+	
