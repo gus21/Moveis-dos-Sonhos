f34c6f69e21c37b020daf09df2e3e8956be902c6
abraaoBaracho <abraaotsurugi@gmail.com>
2018-04-10 12:23:33 -0300

    agora so estar cadastrando um usuario por nickname

--- form.php
+++ form.php
@@ -6,13 +6,23 @@ $pw=$_POST['senha'];
 
 
 $dados = $use."-".$pw."\n";
-
+//abre o arquivo para escrita
 $file = fopen("usuario-senha.txt", 'a+');
+//abre o arquivo em um array
+$arrayUser = file("usuario-senha.txt");
 
-fwrite($file, $dados);
-fclose($file);
+$testUser = in_array($dados, $arrayUser);
+//testa se o usuario ja existe
+if ($testUser == true) {
+	header("location:cadastro.php");
+}else{
 
-header("location:login.php");
+	fwrite($file, $dados);
+	fclose($file);
+	header("location:login.php");
+	
+}
+
 
 
 ?>
