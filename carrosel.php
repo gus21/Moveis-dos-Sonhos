9d6067ddc013984ca2ec6de477694faf91272768
Tuanno <tuannodanyllo26@gmail.com>
2018-05-17 22:48:10 -0300

    melhoria do carossel e pagina do index

--- carrosel.php
+++ carrosel.php
@@ -1,54 +1,47 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<link rel="stylesheet" type="text/css" href="css/carrosel.css">
-<link rel="stylesheet" type="text/css" href="js/carrosel.js">
-
 <!------ Include the above in your HEAD tag ---------->
 
-<body>
-
-
-
-<div class="container">
+<div class="container"> 
     <div class="row">
-		<div class="col-md-12">
-                <div id="Carousel" class="carousel slide">
-                 
-                <ol class="carousel-indicators">
-                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#Carousel" data-slide-to="1"></li>
-                    <li data-target="#Carousel" data-slide-to="2"></li>
-                </ol>
-                 
-                <!-- Carousel items -->
-                <div class="carousel-inner">
-                    
-                <div class="item active">
-                	<div class="row">
-                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/cadeira-de-balanço.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/mesa-centro.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/mesa-xadrez.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/mesa-jantar-marmore.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	</div><!--.row-->
-                </div><!--.item-->
-                 
-                <div class="item">
-                	<div class="row">
-                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/mesa-centro.jpg" alt="Image" style="max-width:100%;"></a></div>
-                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/mesa-jantar-marmore.jpg" alt="Image" style="max-width:100%;"></a></div>
-                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/mesa-xadrez.jpg" alt="Image" style="max-width:100%;"></a></div>
-                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/img_carrossel/cadeira-de-balanço.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	</div><!--.row-->
-                </div><!--.item-->
-                 
-                </div><!--.carousel-inner-->
-                  <a data-slide="prev" href="#Carousel" class="left carousel-control">‹</a>
-                  <a data-slide="next" href="#Carousel" class="right carousel-control">›</a>
-                </div><!--.Carousel-->
-                 
-		</div>
-	</div>
-</div>
-</body>
+    <div id="myCarousel" class="carousel  slide">
+  
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+ 
+ <center>
+  <div class="carousel-inner">
+    <div class="active item"> 
 
+         <img src="img/img_carrossel/promoções.png" >
+
+    </div>
+
+    <div class="item">
+        <img src="img/img_carrossel/cadeira-de-balanço.jpg/" >
+     </div>
+
+    <div class="item">
+        <img src="img/img_carrossel/mesa-centro.jpg" >
+     </div>
+
+    <div class="item"> 
+        <img src="img/img_carrossel/mesa-jantar-marmore.jpg" >
+     </div>
+
+  </div>
+</center>
+
+   <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+</div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -2,11 +2,42 @@
 	
 	include 'cabecario.php';
 	?>
-<body id="indexBackground">
+<body id="indexBackground:snow">
 	<?php
 
 	include 'carrosel.php';
 	?>
+		<?php
+$rows   = array_map('str_getcsv', file('produtos.csv'));
+$header = array_shift($rows);
+$csv    = array();
+foreach($rows as $row) {
+  $csv[] = array_combine($header, $row);
+}
+?>
+<center>
+<div class="container">  
+	<?php if(isset($_SESSION['sucess-produto'])):?>
+		<center><span class="sucess-produto"> Produto Cadastrado com Sucesso </span></center> 
+		<?php unset($_SESSION['sucess-produto']); ?>
+	<?php  endif;?>
+  <?php foreach ($csv as $line => $valores) : ?>
+    <div class="produto">
+    	<a href="produtos_detalhes.php?l=<?=$line?>">
+      <?php echo "<img class=\"img\" src=\"".$valores['IMG']."\">"; ?>
+  		</a>
+       <center>
+       	<span class="detalhes-produto" id= "desc-pro-nome"> <?php echo $valores['NOME'] ?> </span>
+       </center>
+      <!-- <span class="detalhes-produto" id= "desc-pro-descricao"> <?php /*echo $line['DESCRICAO']*/ ?> </span> -->
+      <center>
+      	<span class="detalhes-produto" id= "desc-pro-preco"> <?php echo "R$".$valores['PRECO'] ?> </span>
+      </center>
+      <!-- <span class="detalhes-produto" id= "desc-pro-estoque"> <?php /*echo $line['ESTOQUE']*/ ?> </span> -->
+    </div>
+  <?php endforeach?> 
+</div>
+</center>
 
 	<?php 
 	
