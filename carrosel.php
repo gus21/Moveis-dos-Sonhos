fb65afe0b28e4f970e69d0830da3edb80ed561c9
Tuanno <tuannodanyllo26@gmail.com>
2018-06-07 20:09:27 -0300

    concerteza o erro das imagens na pagina produtos

--- carrosel.php
+++ carrosel.php
@@ -1,51 +1,55 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!-- Include the above in your HEAD tag  -->
+<!------ Include the above in your HEAD tag ---------->
 
-<div class="container">   
-  <?php
-    $rows   = array_map('str_getcsv', file('produtos.csv'));
-    $header = array_shift($rows);
-    $csv    = array();
-    foreach($rows as $row) {
-      $csv[] = array_combine($header, $row);
-    }
+<div class="container" >    <?php
+  $rows   = array_map('str_getcsv', file('produtos.csv'));
+  $header = array_shift($rows);
+  $csv    = array();
+  foreach($rows as $row) {
+    $csv[] = array_combine($header, $row);
+
+  }
   ?>
-    <div class="row">
-      <div id="myCarousel" class="carousel  slide">
+  <div class="row">
+    <div id="myCarousel" class="carousel  slide">
 
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
 
-        <center>
-          <div class="carousel-inner">
-            <div class="active item" style="height: 455px !important;"> 
-              <img src="img/img_carrossel/promoções.png" >
-            </div>
+      <center>
+        <div class="carousel-inner">
+          <div class="active item" style="height: 455px !important;"> 
 
-           <?php foreach ($csv as $line => $valores) : ?>
+           <img src="img/img_carrossel/promoções.png" >
 
-            <div class="item" style="height: 455px !important;"> 
-              <a href="produtos_detalhes.php?l=<?=$line?>">
-               <?php echo "<img src=\"".$valores['IMG']."\">"; ?>
-              </a>
-            </div>
+         </div>
 
-            <?php endforeach?> 
+         <?php foreach ($csv as $line => $valores) : ?>
 
-          </div>
-        </center>
+          <div class="item" style="height: 455px !important;"> 
+            <a href="produtos_detalhes.php?l=<?=$line?>">
+             <?php echo "<img src=\"".$valores['IMG']."\">"; ?>
+           </a>
+         </div>
 
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
-      </div>
-    </div>
+
+       <?php endforeach?> 
+     </div>
+   </center>
+
+   <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
 </div>
+</div>
+</div>
+
+
--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <?php 
+	
 	include 'cabecario.php';
-?>
+	?>
 <body id="indexBackground:snow">
 	<?php
-		include 'carrosel.php';
+
+	include 'carrosel.php';
 	?>
-	<?php
-		$rows   = array_map('str_getcsv', file('produtos.csv'));
-		$header = array_shift($rows);
-		$csv    = array();
-		foreach($rows as $row) {
-  		$csv[] = array_combine($header, $row);
-		}
-	?>
-	<center>
-		<div class="container-index">  
-			<?php if(isset($_SESSION['sucess-produto'])):?>
-			<center><span class="sucess-produto"> Produto Cadastrado com Sucesso </span></center> 
-			<?php unset($_SESSION['sucess-produto']); ?>
-			<?php  endif;?>
-		</div>
-	</center>
+
+<center>
+<div class="container-index">  
+	<?php if(isset($_SESSION['sucess-produto'])):?>
+		<center><span class="sucess-produto"> Produto Cadastrado com Sucesso </span></center> 
+		<?php unset($_SESSION['sucess-produto']); ?>
+	<?php  endif;?>
+
+</div>
+</center>
 
 	<?php 
-		include 'rodape.php';
+	
+	include 'rodape.php';
 	?>
 
 </body>
+
+
