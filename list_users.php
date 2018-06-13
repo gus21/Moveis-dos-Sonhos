2a92f276e8a446a5f1bf589d7a90209b9d11e3b1
luizcsrj <luizcarlosjr1999@gmail.com>
2018-06-13 10:36:37 -0300

    tabela list_users criada

---
+++ list_users.php
@@ -0,0 +1,56 @@
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+    <style>
+  table {
+    border-collapse: collapse;
+    width: 100%;
+    
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+line-height: 2px;
+color:#818181;
+}
+table th {
+    background-color:#337ab7;
+    color: white;
+    font-size:1em;
+    
+}
+table, td, th {    
+    border: 1px solid #ddd;
+    text-align: left;
+}
+th {
+    font-size: 15px;
+}
+tr{
+    width: 100%;
+}
+td{
+    font-size: 20px;
+}
+   </style>
+</head>
+<body>
+    
+    
+    <table class="table">
+        <tr>
+            <th>Email </th>
+            <th>Nome</th>
+            <th>Deletar</th>
+            <th>Editar</th>
+        </tr>      
+        <td><a href=''><i class='fa fa-2x fa-trash sr-icons' title='Deletar Produto'></i></a></td>
+        <td><a href=''><i class='fa fa-2x fa-pencil sr-icons' title='Editar Produto'></i></a></td>
+       
+        </tr>
+    </table>
+</body> 
