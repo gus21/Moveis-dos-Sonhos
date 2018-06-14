bdf6eae6dafeaacdeea92232a35ef89eea9d24f4
Pancosta <dz6.gustav@gmail.com>
2018-06-14 17:41:09 -0300

    arrumado list_users

--- list_users.php
+++ list_users.php
@@ -1,56 +1,21 @@
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-    <style>
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    
-}
-th, td {
-    padding: 8px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
-line-height: 2px;
-color:#818181;
-}
-table th {
-    background-color:#337ab7;
-    color: white;
-    font-size:1em;
-    
-}
-table, td, th {    
-    border: 1px solid #ddd;
-    text-align: left;
-}
-th {
-    font-size: 15px;
-}
-tr{
-    width: 100%;
-}
-td{
-    font-size: 20px;
-}
-   </style>
-</head>
-<body>
-    
-    
-    <table class="table">
+<?php include 'cabecario.php'; ?>
+
+<center>
+    <table id="users_table">
         <tr>
-            <th>Email </th>
             <th>Nome</th>
-            <th>Deletar</th>
-            <th>Editar</th>
-        </tr>      
-        <td><a href=''><i class='fa fa-2x fa-trash sr-icons' title='Deletar Produto'></i></a></td>
-        <td><a href=''><i class='fa fa-2x fa-pencil sr-icons' title='Editar Produto'></i></a></td>
-       
-        </tr>
-    </table>
-</body> 
+            <th>Email </th>
+            <th>Ações</th>
+        </tr>  
+        <td></td>
+        <td></td>    
+        <td width="20%">
+            <center>
+                <a href=''><i class='fa fa-2x fa-trash sr-icons' title='Deletar Usuário'></i></a>
+                <a href=''><i class='fa fa-2x fa-pencil sr-icons' title='Editar Usuário'></i></a>
+            </center>
+        </td>
+    </tr>
+</table> 
+</center>
+<?php include 'rodape.php'; ?>
