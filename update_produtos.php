cd7534efd4eab0beb530d4a0736e92d703295684
Tuanno <tuannodanyllo26@gmail.com>
2018-06-28 22:05:45 -0300

    edit de produtos em andamento

--- update_produtos.php
+++ update_produtos.php
@@ -1,25 +1,28 @@
 <?php 
 session_start();
+$nome = $_POST['nome'];
 $id = $_GET['id'];
-$sql = "SELECT * FROM moveis WHERE id=:id";
-$stmt = $connection->prepare($sql);
-$stmt->execute([":id"=>$id]);
-$person = $stmt->fetch(PDO::FETCH_OBJ);
-if (isset($_POST['nome']) && isset($_POST['preco']) && isset($_POST['categoria']) && isset($_POST['estoque']) && isset($_POST['descricao'])) {
-	
-	$nome 	= $_POST['nome'];
-	$preco 	= $_POST['preco'];
-	$categoria 	= $_POST['categoria'];
-	$estoque 	= $_POST['estoque'];
-	$descricao 	= $_POST['descricao'];
-	$sql = "UPDATE moveis SET nome = :nome , preco = :preco, categoria = :categoria, estoque = :estoque, descricao = :descicao  WHERE id = :id";
-	$stmt = $connection->prepare($sql);
-	if ($stmt->execute([':nome' => $nome , ':preco' => $preco , ':categoria' => $categoria, ':estoque' => $estoque, ':descricao' => $descricao ':id' => $id])) {
-		header('location: produtos.php');
-	}else{
-		echo "Erro";
-	}
+$preco = $_POST['preco'];
+$categoria = $_POST['categoria'];
+$estoque = $_POST['estoque'];
+$descricao = $_POST['descricao'];
+
+require_once('conexao.php');
+$stmt = $pdo->prepare("UPDATE moveis SET  usr_nome = :nome, usr_preco = :preco, usr_categoria = :categoria, usr_estoque = :estoque, usr_descricao = :descricao WHERE usr_id = :id");
+$stmt->BindParam(":nome",$nome);
+$stmt->BindParam(":id",$id);
+$stmt->BindParam(":preco",$preco);
+$stmt->BindParam(":categoria",$categoria);
+$stmt->BindParam(":estoque",$estoque);
+$stmt->BindParam(":descricao",$descricao);
+
+$resultado = $stmt->execute();
+if (isset($resultado)) {	
+	header('location:produtos.php');
+}else{
+	$pdo->errorInfo();
 }
+
 ?>
 
 
